c852187ad1896670ebff6a9946ae87fb0353f9ca
matebe12 <matebe12@gmail.com>
2021-12-05 06:09:17 +0900

    first setting scss, prettier

---
+++ src/App.svelte
@@ -0,0 +1,41 @@
+<script>
+  export let name
+  let jj = 'dkjdhnkjdnkjndkjndkjnkjdn'
+  let jkjj = 'jkknkjnjkn'
+</script>
+
+<style lang="scss">
+</style>
+
+<main>
+  <h1>
+    Hello {name}!
+  </h1>
+
+  <h2>
+    Hello {name}!
+  </h2>
+  <h2>
+    Hello {name}!
+  </h2>
+  <h2>
+    Hello {name}!
+  </h2>
+  <p>
+    Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte
+    apps.
+  </p>
+  <p>
+    Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte
+    apps.
+  </p>
+  <p>
+    Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to buidsddld Svelte
+    apps.
+  </p>
+  <p>
+    Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte
+    apps.
+  </p>
+  <div>kjjsdcssdcfdfsdsf</div>
+</main>
